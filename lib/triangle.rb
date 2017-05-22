7b88fb928574ca428a00b28bb4b54109cf38b783
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-22 20:10:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  def initialize first, second, third
+    self.sides = [first, second, third]
+  end
+
+  def unvalid_triangle?
+    self.sides.any? { |side| side <= 0 } || (self.sides[0] + self.sides[1] <= self.sides[2] || self.sides[0] + self.sides[2] <= self.sides[1] ||
+       self.sides[1] + self.sides[2] <= self.sides[0])
+  end
+
+  def kind
+    raise TriangleError if unvalid_triangle?
+    if sides[0] == sides[1] || sides[0] == sides[2] || sides[1] == sides[2]
+      if sides[0] == sides[1] && sides[1] == sides[2]
+        :equilateral
+      else
+        :isosceles
+      end
+    else
+      :scalene
+    end
+  end
 end
+
+class TriangleError < StandardError; end
